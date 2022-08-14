7d076b2f0b4f35f810c2dfa12f458a32f93c05a5
eric <ericyu0x01@gmail.com>
2022-08-14 23:26:26 +0800

    add core chain testnet config

--- safe_transaction_service/history/management/commands/setup_service.py
+++ safe_transaction_service/history/management/commands/setup_service.py
@@ -292,6 +292,9 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
     EthereumNetwork.KLAY_CYPRESS: [
         ("0xfb1bffC9d739B8D520DaF37dF666da4C687191EA", 93507490, "1.3.0+L2"),
     ],
+    EthereumNetwork.CORE_CHAIN_TESTNET: [
+        ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 930577, "1.3.0+L2"),
+    ],
 }
 
 PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
@@ -421,6 +424,9 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     ],
     EthereumNetwork.KLAY_CYPRESS: [
         ("0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC", 93506870),  # v1.3.0
+    ],
+    EthereumNetwork.CORE_CHAIN_TESTNET: [
+        ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 930394),     # v1.3.0
     ],
 }
 
