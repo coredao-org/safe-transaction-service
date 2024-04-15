67a1fa8df3bddd360fb7be40453b4072a9f854d3
eric <ericyu0x01@gmail.com>
2024-04-15 17:25:29 +0800

    add support for core price

--- safe_transaction_service/tokens/services/price_service.py
+++ safe_transaction_service/tokens/services/price_service.py
@@ -197,6 +197,9 @@ class PriceService:
         except CannotGetPrice:
             return self.coingecko_client.get_kcs_usd_price()
 
+    def get_core_usd_price(self) -> float:
+        return self.coingecko_client.get_core_usd_price()
+
     @cachedmethod(cache=operator.attrgetter("cache_ether_usd_price"))
     @cache_memoize(60 * 30, prefix="balances-get_ether_usd_price")  # 30 minutes
     def get_ether_usd_price(self) -> float:
@@ -289,6 +292,11 @@ class PriceService:
             EthereumNetwork.XDC_APOTHEM_NETWORK,
         ):
             return self.get_xdc_usd_price()
+        elif self.ethereum_network in (
+            EthereumNetwork.CORE_BLOCKCHAIN_MAINNET,
+            EthereumNetwork.CORE_BLOCKCHAIN_TESTNET,
+        ):
+            return self.get_core_usd_price()
         else:
             return self.get_ether_usd_price()
 
